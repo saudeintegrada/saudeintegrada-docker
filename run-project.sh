c954d489d0726299682635c1e0ddeecd9c10f3d8
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-07-03 20:09:27 -0300

    Atualiza imagem docker para hop 1.2

--- run-project.sh
+++ run-project.sh
@@ -1,3 +1,4 @@
+#--env HOP_PLUGIN_BASE_FOLDERS=./plugins,/files/misc/plugins \
 docker run -it --rm \
   --env HOP_LOG_LEVEL=Basic \
   --env HOP_FILE_PATH='${PROJECT_HOME}/ETL-Covid19/Publicados/WF-NotificaCovid-Extras.hwf' \
@@ -7,8 +8,7 @@ docker run -it --rm \
   --env HOP_ENVIRONMENT_CONFIG_FILE_NAME_PATHS=/files/prod-config.json \
   --env HOP_SHARED_JDBC_FOLDER=/files/misc/drivers \
   --env HOP_RUN_CONFIG=local \
-  --env HOP_PLUGIN_BASE_FOLDERS=./plugins,/files/misc/plugins \
   --env HOP_RUN_PARAMETERS=PARAM_LOG_MESSAGE=Hello,PARAM_WAIT_FOR_X_MINUTES=1 \
   -v /mnt/e/Arquivos/Documentos/Github/BI/saudeintegrada-dw:/files \
   --name my-simple-hop-container \
-  saudeintegrada/hop
+  saudeintegrada/hop-cpython:1.2
